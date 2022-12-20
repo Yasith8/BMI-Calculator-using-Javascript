bf2daa4e1dfb24b393b74159860a9a91f61e8868
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-20 22:35:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 var weight, height, measure, bmi, error ;
 
+
 function calculate() {
+	
 	weight = document.getElementById("weight").value;
+	
 	height = document.getElementById("height").value;
+	
 	error = "Please enter some values";
+	
 	height /= 100;
+	
 	height *= height;
+	
 	bmi = weight/height;
+	
 	bmi = bmi.toFixed(1);
 
+	
+	
 	if (bmi <= 18.4) {
+		
 		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
-	} else if (bmi >= 18.5 && bmi <= 24.9) {
+	}
+	else if (bmi >= 18.5 && bmi <= 24.9) {
+		
 		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
-	} else if (bmi >= 25 && bmi <= 29.9) {
+	}
+	else if (bmi >= 25 && bmi <= 29.9) {
+		
 		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
-	} else if (bmi >= 30) {
+	}
+	else if (bmi >= 30) {
+		
 		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
 	}
 	
+	
 
 	if (weight === 0 ) {
+		
 		document.getElementById("results").innerHTML = error;
-	} else if (height === 0){
+	} 
+	else if (height === 0){
+		
 		document.getElementById("results").innerHTML = error;
 	}
 	 else {
@@ -30,6 +51,7 @@ function calculate() {
 		document.getElementById("results").innerHTML = measure;
 	}
 	if (weight < 0) {
+		
 		document.getElementById("results").innerHTML = "Negative Values not Allowed";
 	}
 }
